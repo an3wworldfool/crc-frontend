7d0177e81c5ec3ed4ef6283d987a59123926de0d
an3wworldfool <ing.germanspdf@gmail.com>
2024-07-17 19:29:19 -0700

    Modified github actions, included playwright tests

--- public/script.js
+++ public/script.js
@@ -23,8 +23,14 @@ document.getElementsByClassName("devto-grid")[0].onclick = function(){
 
 
 fetch("https://u71izje6c9.execute-api.us-west-2.amazonaws.com/Prod/views")
-  .then((response) => response.json())
+  .then((response) => {
+    console.log(response
+      
+    )
+    response.json()
+  })
   .then((json) => {
+    console.log(json)
     res = JSON.parse(json["body"]);
     console.log(res);
     counter.innerText = res["views"];
